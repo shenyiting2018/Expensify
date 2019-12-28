c62941dd74e98890848de5d314491022d4df741b
Yiting <shenyiting2018@gmail.com>
2019-12-27 17:24:29 -0800

    90

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,14 +1,44 @@
 import { createStore } from 'redux';
 
+const add = ({ a, b }, c) => {
+    return a + b + c;
+};
+
+console.log(add({ a: 1, b: 12}, 100));
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
-                count: state.count + 1
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
             return {
-                count: state.count - 1
+                count: state.count - action.decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         case 'RESET':
             return {
@@ -19,18 +49,18 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-    type: 'INCREMENT'
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'RESET'
-})
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(incrementCount());
 
-console.log(store.getState());
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10}));
+
+store.dispatch(setCount({ count: 101 }));
